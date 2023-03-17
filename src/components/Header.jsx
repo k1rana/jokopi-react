9307f2c7fb1fb59eddbfbf1d1ab362adba2d2f34
ahann <ahannshi@gmail.com>
2023-03-17 08:53:49 +0700

    continue main page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import icon from '../assets/jokopi.svg';
+import icon from "../assets/jokopi.svg";
 
 class Header extends Component {
-    render(){
-        return(
-            <header className="px-22 flex justify-between"><div className="py-10 font-extrabold flex flex-row justify-center gap-4"> <img src={icon} alt="logo" width="30px" /> <h1 className="text-xl">jokopi.</h1>
-        </div><nav className="py-10 hidden lg:flex flex-row gap-8 justify-center">
-            <li className="list-none"><a href="/" className="font-bold text-[#6A4029]">Home</a></li>
-            <li className="list-none"><a href="/product.html">Product</a></li>
-            <li className="list-none"><a href="#">Your Cart</a></li>
-            <li className="list-none"><a href="/history.html">History</a></li>
+  render() {
+    return (
+      <header className="sticky top-0 z-50 px-22 flex justify-between bg-white border-b-2 border-gray-100">
+        <div className="py-8 font-extrabold flex flex-row justify-center gap-4">
+          <img src={icon} alt="logo" width="30px" />
+          <h1 className="text-xl">jokopi.</h1>
+        </div>
+        <nav className="py-8 hidden lg:flex flex-row gap-8 justify-center">
+          <li className="list-none">
+            <a href="/" className="font-bold text-[#6A4029]">
+              Home
+            </a>
+          </li>
+          <li className="list-none">
+            <a href="/product.html">Product</a>
+          </li>
+          <li className="list-none">
+            <a href="#">Your Cart</a>
+          </li>
+          <li className="list-none">
+            <a href="/history.html">History</a>
+          </li>
         </nav>
-        <div className="flex-row gap-3 hidden lg:flex items-center select-none py-8">
-            <Link to="/login" className='pr-9 font-semibold'>Login</Link>
-            <button className='rounded-[25px] bg-secondary px-10 text-tertiary font-semibold h-full'>Sign Up</button>
-            </div></header>);
-    }
+        <div className="flex-row gap-3 hidden lg:flex items-center select-none py-6">
+          <Link to="/login" className="pr-9 font-semibold">
+            Login
+          </Link>
+          <Link to="/register">
+            <button className="rounded-[25px] bg-secondary px-10 text-tertiary font-semibold py-2">
+              Sign Up
+            </button>
+          </Link>
+        </div>
+      </header>
+    );
+  }
 }
 
 export default Header;
--- src/pages/Mainpage.jsx
+++ src/pages/Mainpage.jsx
@@ -1,14 +1,20 @@
-import React, {
-  Component,
-  Fragment,
-} from 'react';
+import React, { Component, Fragment } from "react";
 
 // assets icons
-import checkCircle from '../assets/icons/check-circle.svg';
+import checkCircle from "../assets/icons/check-circle.svg";
+import checkIcon from "../assets/icons/check.svg";
+import mapImage from "../assets/images/global.svg";
+import amazonLogo from "../assets/images/partners/amazon.svg";
+import discordLogo from "../assets/images/partners/discord.svg";
+import netflixLogo from "../assets/images/partners/netflix.svg";
+import redditLogo from "../assets/images/partners/reddit.svg";
+import spotifyLogo from "../assets/images/partners/spotify.svg";
+import productImage1 from "../assets/images/product-1.webp";
 // assets images
-import provideImage from '../assets/images/team-work.webp';
-import Footer from '../components/Footer';
-import Header from '../components/Header';
+import provideImage from "../assets/images/team-work.webp";
+// components
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 
 class Mainpage extends Component {
   state = {
@@ -60,6 +66,155 @@ class Mainpage extends Component {
                   </li>
                 ))}
               </ul>
+            </div>
+          </section>
+          <section className="px-22 py-20">
+            <div className="flex flex-col items-center">
+              <h2 className="text-4xl text-quartenary font-semibold mb-5 text-center">
+                Here is People’s Favorite
+              </h2>
+              <p className="text-base text-gray-700 text-center">
+                Let’s choose and have a bit taste of poeple’s favorite. It might
+                be yours too!
+              </p>
+            </div>
+            <div className="flex flex-row justify-center gap-12 mt-20">
+              <div className="flex-1 flex flex-col justify-center border-gray-400 border rounded-xl px-5 py-12 items-center gap-5 text-base">
+                <img
+                  src={productImage1}
+                  alt=""
+                  width="140px"
+                  className="rounded-full mb-7"
+                />
+                <h3 className="text-lg font-medium">Hazelnut Latte</h3>
+                <ul className="flex flex-col gap-5 mb-20">
+                  <li className="flex items-center gap-4">
+                    <img src={checkIcon} alt="" /> Hazelnut Syrup
+                  </li>
+                  <li className="flex items-center gap-4">
+                    <img src={checkIcon} alt="" /> Wanilla Whipped Cream
+                  </li>
+                  <li className="flex items-center gap-4">
+                    <img src={checkIcon} alt="" /> Ice / Hot
+                  </li>
+                  <li className="flex items-center gap-4">
+                    <img src={checkIcon} alt="" /> Sliced Banana on Top
+                  </li>
+                </ul>
+                <p className="font-medium text-2xl">IDR 25.000</p>
+                <button className="bg-secondary text-tertiary px-9 py-3 rounded-3xl font-bold">
+                  Order Now
+                </button>
+              </div>
+              <div className="flex-1 flex flex-col justify-center border-gray-400 border rounded-xl px-5 py-12 items-center gap-5 text-base">
+                <img
+                  src={productImage1}
+                  alt=""
+                  width="140px"
+                  className="rounded-full mb-7"
+                />
+                <h3 className="text-lg font-medium">Hazelnut Latte</h3>
+                <ul className="flex flex-col gap-5 mb-20">
+                  <li className="flex items-center gap-4">
+                    <img src={checkIcon} alt="" /> Hazelnut Syrup
+                  </li>
+                  <li className="flex items-center gap-4">
+                    <img src={checkIcon} alt="" /> Wanilla Whipped Cream
+                  </li>
+                  <li className="flex items-center gap-4">
+                    <img src={checkIcon} alt="" /> Ice / Hot
+                  </li>
+                  <li className="flex items-center gap-4">
+                    <img src={checkIcon} alt="" /> Sliced Banana on Top
+                  </li>
+                </ul>
+                <p className="font-medium text-2xl">IDR 25.000</p>
+                <button className="bg-secondary text-tertiary px-9 py-3 rounded-3xl font-bold">
+                  Order Now
+                </button>
+              </div>
+              <div className="flex-1 flex flex-col justify-center border-gray-400 border rounded-xl px-5 py-12 items-center gap-5 text-base">
+                <img
+                  src={productImage1}
+                  alt=""
+                  width="140px"
+                  className="rounded-full mb-7"
+                />
+                <h3 className="text-lg font-medium">Hazelnut Latte</h3>
+                <ul className="flex flex-col gap-5 mb-20">
+                  <li className="flex items-center gap-4">
+                    <img src={checkIcon} alt="" /> Hazelnut Syrup
+                  </li>
+                  <li className="flex items-center gap-4">
+                    <img src={checkIcon} alt="" /> Wanilla Whipped Cream
+                  </li>
+                  <li className="flex items-center gap-4">
+                    <img src={checkIcon} alt="" /> Ice / Hot
+                  </li>
+                  <li className="flex items-center gap-4">
+                    <img src={checkIcon} alt="" /> Sliced Banana on Top
+                  </li>
+                </ul>
+                <p className="font-medium text-2xl">IDR 25.000</p>
+                <button className="bg-secondary text-tertiary px-9 py-3 rounded-3xl font-bold">
+                  Order Now
+                </button>
+              </div>
+            </div>
+          </section>
+          <section className="px-22 py-20">
+            <div className="flex flex-col items-center mb-20">
+              <h2 className="text-4xl text-quartenary font-semibold mb-5 text-center">
+                Visit Our Store in
+                <br />
+                the Spot on the Map Below
+              </h2>
+              <p className="text-base text-gray-700 text-center">
+                See our store in every city on the spot and spen your good day
+                there. See you soon!
+              </p>
+            </div>
+            <div className="mt-10">
+              <img src={mapImage} alt="global map" />
+            </div>
+          </section>
+          <section className="px-22 py-20">
+            <div className="flex flex-col items-center mb-20">
+              <h2 className="text-4xl text-quartenary font-semibold mb-5 text-center">
+                Our Partner
+              </h2>
+            </div>
+            <div className="flex flex-row justify-center items-center gap-12 ">
+              <img
+                src={netflixLogo}
+                alt=""
+                width="190px"
+                className="w-[15%] aspect-[3/2] object-contain grayscale opacity-20 duration-300 hover:filter-none hover:opacity-100"
+              />
+              <img
+                src={redditLogo}
+                alt=""
+                width="190px"
+                className="w-[15%] aspect-[3/2] object-contain grayscale opacity-20 duration-300 hover:filter-none hover:opacity-100"
+              />
+              <img
+                src={amazonLogo}
+                alt=""
+                width="190px"
+                className="w-[15%] aspect-[3/2] object-contain grayscale opacity-20 duration-300 hover:filter-none hover:opacity-100"
+              />
+              <img
+                src={discordLogo}
+                alt=""
+                width="190px"
+                className="w-[15%] aspect-[3/2] object-contain grayscale opacity-20 duration-300 hover:filter-none hover:opacity-100"
+              />
+              <img
+                src={spotifyLogo}
+                alt=""
+                width="190px"
+                className="w-[15%] aspect-[3/2] object-contain grayscale opacity-20 duration-300 hover:filter-none hover:opacity-100"
+              />
             </div>
           </section>
         </main>
