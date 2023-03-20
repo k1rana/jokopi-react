0174c2cdebebc876c6afb7b5564517f0150e3454
justcallmeahann <krisssupervector077@gmail.com>
2023-03-20 09:16:57 +0700

    footer login changed

--- src/components/AuthFooter.jsx
+++ src/components/AuthFooter.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 
 import { Link } from "react-router-dom";
 
@@ -7,20 +7,20 @@ import igLogo from "../assets/icons/instagram.svg";
 import twLogo from "../assets/icons/twitter.svg";
 import logo from "../assets/jokopi.svg";
 
-class Footer extends Component {
-  render() {
-    return (
-      <footer className="bg-[#F8F8F8] px-5 lg:px-22 text-[#4f5665]">
-        <div className="py-5  md:py-20"></div>
+const AuthFooter = () => {
+  return (
+    <>
+      <footer className="bg-[#F8F8F8] px-5 lg:px-16 text-[#4f5665]">
+        <div className="py-5  md:py-10"></div>
         <div className="flex flex-col-reverse gap-12 md:flex-row">
-          <div className="flex flex-col gap-4 md:flex-[2_2_0%]">
+          <div className="flex flex-col gap-4 md:max-w-[50%]">
             <Link to="/">
               <div className="font-extrabold flex flex-row gap-2">
                 <img src={logo} alt="logo" width="30px" />{" "}
                 <h1 className="text-xl text-black">jokopi.</h1>
               </div>
             </Link>
-            <div className="md:w-96">
+            <div className="">
               jokopi is a store that sells some good meals, and especially
               coffee. We provide high quality beans
             </div>
@@ -28,85 +28,88 @@ class Footer extends Component {
               <a
                 href="#"
                 className="bg-tertiary h-[35px] w-[35px] flex items-center justify-center rounded-full"
+                aria-label="Facebook"
               >
                 <img src={fbLogo} alt="" />
               </a>
               <a
                 href="#"
                 className="bg-tertiary h-[35px] w-[35px] flex items-center justify-center rounded-full"
+                aria-label="Instagram"
               >
                 <img src={igLogo} alt="" />
               </a>
               <a
                 href="#"
                 className="bg-tertiary h-[35px] w-[35px] flex items-center justify-center rounded-full"
+                aria-label="Twitter"
               >
                 <img src={twLogo} alt="" width="120%" className="w-16" />
               </a>
             </div>
             <div className="copyright">(c) 2023 jokopi</div>
           </div>
-          <nav className="flex flex-row gap-10 md:flex-1">
+          <nav className="flex flex-row lg:flex-col gap-10 md:flex-1">
             <div className="flex-1 flex flex-col gap-5">
               <div className="grid-item">
-                <h4 className="font-bold">Product</h4>
+                <p className="font-bold">Product</p>
               </div>
-              <div className="flex flex-col gap-2">
-                <div className="grid-item">
-                  <a href="https://" target="_blank" rel="noopener noreferrer">
+              <div className="flex flex-col gap-2  lg:flex-row lg:gap-x-10 lg:gap-y-4 flex-wrap w-full">
+                <div className="grid-item flex-1">
+                  <a href="#" target="_blank" rel="noopener noreferrer">
                     {" "}
                     Download
                   </a>
                 </div>
-                <div className="grid-item">
-                  <a href="https://" target="_blank" rel="noopener noreferrer">
+                <div className="grid-item flex-1">
+                  <a href="#" target="_blank" rel="noopener noreferrer">
                     Pricing
                   </a>
                 </div>
-                <div className="grid-item">
-                  <a href="https://" target="_blank" rel="noopener noreferrer">
+                <div className="grid-item flex-1">
+                  <a href="#" target="_blank" rel="noopener noreferrer">
                     Locations
                   </a>
                 </div>
-                <div className="grid-item">
-                  <a href="https://" target="_blank" rel="noopener noreferrer">
+                <div className="grid-item flex-1">
+                  <a href="#" target="_blank" rel="noopener noreferrer">
                     Countries
                   </a>
                 </div>
-                <div className="grid-item">
-                  <a href="https://" target="_blank" rel="noopener noreferrer">
+                <div className="grid-item flex-1">
+                  <a href="#" target="_blank" rel="noopener noreferrer">
                     Blog
                   </a>
                 </div>
               </div>
             </div>
-            <div className="flex-1 flex flex-col gap-5">
+            <div className="flex-1 flex flex-col gap-2">
               <div className="grid-item">
-                <h4 className="font-bold">Engage</h4>
+                <p className="font-bold">Engage</p>
               </div>
-              <div className="flex flex-col gap-2">
-                <div className="grid-item">
-                  <a href="https://" target="_blank" rel="noopener noreferrer">
-                    Coffee Shop ?
+              <div className="flex flex-col gap-2  lg:flex-row lg:gap-x-10 lg:gap-y-4 flex-wrap w-full text-base">
+                <div className="grid-item flex-1 min-w-[5rem]">
+                  <a href="#" target="_blank" rel="noopener noreferrer">
+                    Coffee Shop?
                   </a>
                 </div>
-                <div className="grid-item">
-                  <a href="https://" target="_blank" rel="noopener noreferrer">
+                <div className="grid-item flex-1 min-w-[5rem]">
+                  <a href="#" target="_blank" rel="noopener noreferrer">
                     FAQ
                   </a>
                 </div>
-                <div className="grid-item">
-                  <a href="https://" target="_blank" rel="noopener noreferrer">
+                <div className="grid-item flex-1">
+                  <a href="#" target="_blank" rel="noopener noreferrer">
                     About Us
                   </a>
                 </div>
-                <div className="grid-item">
-                  <a href="https://" target="_blank" rel="noopener noreferrer">
+                <div className="grid-item flex-1">
+                  <a href="#" target="_blank" rel="noopener noreferrer">
                     Privacy Policy
                   </a>
                 </div>
-                <div className="grid-item">
-                  <a href="https://" target="_blank" rel="noopener noreferrer">
+                <div className="grid-item flex-1">
+                  <a href="#" target="_blank" rel="noopener noreferrer">
                     Terms of Services
                   </a>
                 </div>
@@ -116,8 +119,8 @@ class Footer extends Component {
         </div>
         <div className="py-5"></div>
       </footer>
-    );
-  }
-}
+    </>
+  );
+};
 
-export default Footer;
+export default AuthFooter;
---
+++ src/pages/Products.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Header from "../components/Header";
+
+function Products() {
+  return (
+    <>
+      <Header />
+      <main className="flex flex-col lg:flex-row">
+        <section>qrwr</section>
+        <section>qweew</section>
+      </main>
+    </>
+  );
+}
+export default Products;
