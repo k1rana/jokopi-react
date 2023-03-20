a1fa86d0371bd753ab578103c55abae2eb7761b4
justcallmeahann <krisssupervector077@gmail.com>
2023-03-20 21:08:34 +0700

    add link to list products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 import images from "../assets/images/person-with-a-coffee.webp";
 import productPlaceholder from "../assets/images/placeholder-image.webp";
@@ -15,7 +15,7 @@ const promos = [
   },
   {
     name: "Lorem ipsum",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
   },
   {
     name: "Lorem ipsum",
@@ -94,28 +94,29 @@ function Products(props) {
           </nav>
           <section className="grid grid-cols-2 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-3 justify-items-center content-around gap-3 gap-y-16 mt-10">
             {products.map((product, idx) => (
-              <section
-                className="relative w-36 bg-white shadow-xl p-5 rounded-3xl"
-                key={idx}
-              >
-                <img
-                  src={product.img ?? productPlaceholder}
-                  alt=""
-                  className="aspect-square rounded-full object-cover mt-[-50%] w-full mb-3 shadow-lg"
-                />
-                <div className="absolute top-0 right-0 bg-white p-2 rounded-3xl font-extrabold text-lg">
-                  20%
-                </div>
-                <div className="flex flex-col gap-5 content-between text-center">
-                  <p className="font-black text-lg">{product.name}</p>
-                  <p className="font-bold end text-tertiary">
-                    IDR{" "}
-                    {product.price
-                      .toString()
-                      .replace(/\B(?=(\d{3})+(?!\d))/g, ".")}
-                  </p>
-                </div>
-              </section>
+              <Link to={`/products/${idx}`} key={idx}>
+                <section className="relative w-36 bg-white shadow-xl p-5 rounded-3xl">
+                  <img
+                    src={product.img ?? productPlaceholder}
+                    alt=""
+                    className="aspect-square rounded-full object-cover mt-[-50%] w-full mb-3 shadow-lg"
+                  />
+                  <div className="absolute top-0 right-0 bg-white p-2 rounded-3xl font-extrabold text-lg">
+                    20%
+                  </div>
+                  <div className="flex flex-col gap-5 content-between text-center">
+                    <p className="font-black text-lg min-h-[102px]">
+                      {product.name}
+                    </p>
+                    <p className="font-bold end text-tertiary">
+                      IDR{" "}
+                      {product.price
+                        .toString()
+                        .replace(/\B(?=(\d{3})+(?!\d))/g, ".")}
+                    </p>
+                  </div>
+                </section>
+              </Link>
             ))}
           </section>
           <section className="my-6 text-tertiary">
