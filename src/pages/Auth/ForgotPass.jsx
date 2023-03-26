64cbfdf6486bb914b517c01b6911f86d2cbaac21
justcallmeahann <80017838+justcallmeahann@users.noreply.github.com>
2023-03-26 23:35:40 +0700

    fix spacing, add forgot pass handler

--- src/pages/Auth/ForgotPass.jsx
+++ src/pages/Auth/ForgotPass.jsx
@@ -1,12 +1,41 @@
 import React from 'react';
 
+import toast from 'react-hot-toast';
 import { Link } from 'react-router-dom';
 
 import icon from '../../assets/jokopi.svg';
+import { forgotPass } from '../../utils/dataProvider/auth';
 import useDocumentTitle from '../../utils/documentTitle';
 
 const ForgotPass = () => {
   useDocumentTitle("Forgot Password");
+
+  const controller = React.useMemo(() => new AbortController(), []);
+  const [email, setEmail] = React.useState("");
+
+  function forgotPassHandler(e) {
+    e.preventDefault(); // preventing default submit
+    toast.dismiss(); // dismiss all toast
+
+    toast.promise(
+      forgotPass(email, controller).then((res) => {
+        // console.log(res.data.data.token);
+        return res.data;
+      }),
+      {
+        loading: "Please wait a moment",
+        success: "We sent a code to your email!",
+        error: (e) => {
+          return e.response.data.msg;
+        },
+      }
+    );
+  }
+
+  function handleChange(e) {
+    return setEmail(e.target.value);
+  }
+
   return (
     <>
       <header className="flex justify-center mb-10">
@@ -34,20 +63,32 @@ const ForgotPass = () => {
           <div>
             <input
               type="text"
-              name=""
+              name="email"
               id="email"
               className="border-gray-400 border-2 rounded-2xl p-3 w-full mt-2"
               placeholder="Enter your email adress to get link"
+              value={email}
+              onChange={handleChange}
             />
           </div>
           <button
             type="submit"
             className="w-full text-tertiary bg-secondary focus:ring-4 focus:outline-none focus:ring-primary-300 font-bold rounded-2xl text-lg p-3 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 shadow-xl"
+            onClick={forgotPassHandler}
           >
             Send
           </button>
-          <div className="h-16"></div>
         </form>
+        <section className="text-center mt-10 space-y-2">
+          <p>Click here if you didn’t receive any link in 2 minutes</p>
+          <p className="font-bold">01:52</p>
+          <button
+            type="submit"
+            className="w-full text-white bg-tertiary focus:ring-4 focus:outline-none focus:ring-primary-300 font-bold rounded-2xl text-lg p-3 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 shadow-xl"
+          >
+            Resend Link
+          </button>
+        </section>
       </section>
     </>
   );
--- src/pages/Auth/index.jsx
+++ src/pages/Auth/index.jsx
@@ -12,7 +12,7 @@ const Auth = () => {
           <section className="w-0 min-h-screen lg:w-1/2 lg:block hidden bg-cover bg-center bg-main"></section>
           <section className="lg:w-1/2 bg-main bg-cover bg-center bg-black/70 lg:bg-white lg:bg-none lg:text-black">
             <div className=" px-4 py-7 lg:px-16 flex justify-center flex-col">
-              <div className="bg-white px-5 py-6 lg:p-0 rounded-xl min-h-[calc(100vh-3.5rem)]">
+              <div className="bg-white px-5 py-6 lg:p-0 rounded-xl min-h-screen">
                 <Outlet />
               </div>
             </div>
